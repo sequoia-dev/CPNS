fc58d48230ec4ad22d2ae6aa842fad61c3c53075
ccattin-dev <75748278+ccattin-dev@users.noreply.github.com>
2020-12-09 19:23:17 +0100

    Update code_simulation.py

--- code_simulation.py
+++ code_simulation.py
@@ -4,6 +4,29 @@
 Mini projet Python équation de transport
 """
 
+#Importation des librairies
+
+import numpy as np
+
+#Definition des fonctions
 
 def ctilde(x):
+    """
+    Retourne la valeur de ctilde(x) comme donnée dans l'énoncé
+    c(0,x) = ctilde(x)
+    """
     
+    c = np.max(0,np.exp(-(x-0.2)**2/0.005 - 1 ))
+    
+    return c
+
+def c(v,dt,dx):
+    
+
+
+
+
+#Programme principale
+
+if __name__ == '__main__':
+    pass
